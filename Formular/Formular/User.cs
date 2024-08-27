ddc0a922a4c1ad3fbcb0d5ad016d3bb87de7d8e7
IA23 a-acikgozy <acikgoezy@bzz.ch>
2024-08-27 09:23:47 +0200

    made Database

--- Formular/Formular/User.cs
+++ Formular/Formular/User.cs
@@ -10,6 +10,8 @@ namespace Formular
     {
         //Test
 
+        public int Id {  get; set; }
+
 
         public string Name { get; set; }
 
---
+++ Formular/Formular/UserDB.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formular
+{
+    public class UserDB : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=YAGMUR\SQLEXPRESS;Database=UserDb;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+
+
+}
